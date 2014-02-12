55d42fe87991378baed035e7002e2d120e25be16
kasperisager <kasperisager@gmail.com>
2014-02-12 10:30:56 -0500

    Remove dropdown.js as it's not used out-of-the-box

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ gulp.task('scripts', function () {
     // 'bower_components/bootstrap/js/button.js',
     // 'bower_components/bootstrap/js/carousel.js',
     'bower_components/bootstrap/js/collapse.js',
-    'bower_components/bootstrap/js/dropdown.js',
+    // 'bower_components/bootstrap/js/dropdown.js',
     // 'bower_components/bootstrap/js/modal.js',
     // 'bower_components/bootstrap/js/tooltip.js',
     // 'bower_components/bootstrap/js/popover.js',
