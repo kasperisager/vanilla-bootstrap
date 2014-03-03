452b439df14f94f1c5e066b3b647f8fdbf5df672
kasperisager <kasperisager@gmail.com>
2014-03-02 21:51:39 -0500

    Tweak theme meta

--- about.php
+++ about.php
@@ -1,12 +1,13 @@
-<?php if (!defined('APPLICATION')) exit();
+<?php
 
 $ThemeInfo['bootstrap'] = array(
   'Name'        => 'Bootstrap',
   'Description' => "<strong>NOW INCLUDES THE BOOTSWATCH THEMES!</strong> Bootstrap for Vanilla has been rewritten from scratch on top of <a href='http://getbootstrap.com'>Bootstrap 3</a> in this second installation of the most popular theme for Vanilla Forums. Kickstart your community with a fresh and solid theme that is ready to be customized to your heart's desires.",
   'Version'     => '2.1.0',
-  'Author'      => 'Kasper K. Isager',
+  'Author'      => 'Kasper Kronborg Isager',
   'AuthorEmail' => 'kasperisager@gmail.com',
   'AuthorUrl'   => 'https://github.com/kasperisager',
+  'License'     => 'MIT',
   'RequiredApplications' => array('Vanilla' => '2.2.x'),
 
   'Options' => array(
