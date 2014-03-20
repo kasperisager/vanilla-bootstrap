3aa3c4e9dd1037eaf0195349c623b13bd679ad56
kasperisager <kasperisager@gmail.com>
2014-03-20 11:25:26 -0400

    Attach JS events as early as possible

--- js/main.js
+++ js/main.js
@@ -1,4 +1,48 @@
 ;(function ($, window, document, undefined) {
+  'use strict';
+
+  var spinner = {
+    lines  : 11
+  , radius : 5
+  , length : 5
+  , width  : 2
+  };
+
+  // Attach spinners to the .InProgress element in flyouts using Spin.js
+  $(document).on('click', '.ToggleFlyout', function (e) {
+    $('.InProgress', e.currentTarget).spin(spinner);
+  });
+
+  var backdrop = '.Overlay'
+    , dialog   = '.Overlay > .Popup';
+
+  $(document)
+    // Show the modal backdrop, but hide the actual modal dialog while it's
+    // loading.
+    .on('popupLoading', function (e) {
+      $('body').addClass('modal-open');
+      $(dialog).addClass('fade');
+    })
+    // Fade in the modal dialog when it's time to reveal it.
+    .on('popupReveal', function (e) {
+      $(dialog).addClass('in');
+    })
+    // When it's time to close the modal, first fade out the modal dialog,
+    // then fade out the modal backdrop, and lastly remove the entire modal
+    // from the DOM.
+    .on('popupClose', function (e) {
+      $(dialog).removeClass('in');
+      setTimeout(function () { $('body').removeClass('modal-open'); }, 150);
+      setTimeout(function () { $(backdrop).remove(); }, 300);
+    });
+
+  // When only a confirmation modal is shown, the "popupLoading" and
+  // "popupReveal" events are never triggered. Manually trigger them to make
+  // sure that the modal is actually shown.
+  $(document).on('click', 'a.Delete, a.DeleteComment, a.PopConfirm', function (e) {
+    $('body').trigger('popupLoading');
+    setTimeout(function () { $('body').trigger('popupReveal'); }, 150);
+  });
 
   $(function () {
 
@@ -7,48 +51,6 @@
     // if the button is later shown.
     $('.Button.Hidden').removeClass('Hidden').hide();
 
-    // Attach spinners to the .InProgress element in flyouts using Spin.js
-    $(document).on('click', '.ToggleFlyout', function (e) {
-      var spinner = {
-        lines  : 11
-      , radius : 5
-      , length : 5
-      , width  : 2
-      };
-
-      $('.InProgress', e.currentTarget).spin(spinner);
-    });
-
-    var backdrop = '.Overlay'
-      , dialog   = '.Overlay > .Popup';
-
-    $(document)
-      // Show the modal backdrop, but hide the actual modal dialog while it's
-      // loading.
-      .on('popupLoading', function (e) {
-        $('body').addClass('modal-open');
-        $(dialog).addClass('fade');
-      })
-      // Fade in the modal dialog when it's time to reveal it.
-      .on('popupReveal', function (e) {
-        $(dialog).addClass('in');
-      })
-      // When it's time to close the modal, first fade out the modal dialog,
-      // then fade out the modal backdrop, and lastly remove the entire modal
-      // from the DOM.
-      .on('popupClose', function (e) {
-        $(dialog).removeClass('in');
-        setTimeout(function () { $('body').removeClass('modal-open'); }, 150);
-        setTimeout(function () { $(backdrop).remove(); }, 300);
-      });
-
-    // When only a confirmation modal is shown, the "popupLoading" and
-    // "popupReveal" events are never triggered. Manually trigger them to make
-    // sure that the modal is actually shown.
-    $(document).on('click', 'a.Delete, a.DeleteComment, a.PopConfirm', function (e) {
-      $('body').trigger('popupLoading');
-      setTimeout(function () { $('body').trigger('popupReveal'); }, 150);
-    });
   });
 
   /**
