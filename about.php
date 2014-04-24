0a455a7a057d0f30b868883f45eee1b626573887
kasperisager <kasperisager@gmail.com>
2014-04-24 14:41:00 -0400

    Old news

--- about.php
+++ about.php
@@ -2,7 +2,7 @@
 
 $ThemeInfo['bootstrap'] = array(
   'Name'        => 'Bootstrap',
-  'Description' => "<strong>NOW INCLUDES THE BOOTSWATCH THEMES!</strong> Bootstrap for Vanilla has been rewritten from scratch on top of <a href='http://getbootstrap.com'>Bootstrap 3</a> in this second installation of the most popular theme for Vanilla Forums. Kickstart your community with a fresh and solid theme that is ready to be customized to your heart's desires.",
+  'Description' => "Bootstrap for Vanilla has been rewritten from scratch on top of <a href='http://getbootstrap.com'>Bootstrap 3</a> in this second installation of the most popular theme for Vanilla Forums. Kickstart your community with a fresh and solid theme that is ready to be customized to your heart's desires.",
   'Version'     => '2.1.4',
   'Url'         => 'https://github.com/kasperisager/vanilla-bootstrap',
   'Author'      => 'Kasper Kronborg Isager',
