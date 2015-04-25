3fe535563d05a417793b5a2e7ecb871ca8525a43
Kasper Isager <kasperisager@gmail.com>
2015-04-25 20:56:24 +0200

    Bump version to 2.5.1

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
 $ThemeInfo['bootstrap'] = array(
   'Name'        => 'Bootstrap',
   'Description' => "Bootstrap for Vanilla has been rewritten from scratch on top of <a href='http://getbootstrap.com'>Bootstrap 3</a> in this second installation of the most popular theme for Vanilla Forums. Kickstart your community with a fresh and solid theme that is ready to be customized to your heart's desires.",
-  'Version'     => '2.5.0',
+  'Version'     => '2.5.1',
   'Url'         => 'https://github.com/kasperisager/vanilla-bootstrap',
   'Author'      => 'Kasper Kronborg Isager',
   'AuthorEmail' => 'kasperisager@gmail.com',
